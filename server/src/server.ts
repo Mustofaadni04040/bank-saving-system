bd2cb103ec923b78a74700506a0279f87a7cad77
mustofa adny <adniadni04@gmail.com>
2025-11-29 15:09:07 +0700

    setup mongoDB in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,17 @@
 import type e = require("express");
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./utils/db");
 
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+const corsOtions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+app.use(cors(corsOtions));
 
 app.get("/", (req: e.Request, res: e.Response) =>
   res.send("Bank Saving API is running")
@@ -14,4 +19,7 @@ app.get("/", (req: e.Request, res: e.Response) =>
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running on port ${PORT}`);
+});
