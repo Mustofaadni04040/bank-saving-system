292a2d7e9c1a53ad5a5031eaabb2cc6f9f7f182c
mustofa adny <adniadni04@gmail.com>
2025-12-01 14:30:25 +0700

    feat: create adminAuth middleware for admin route

---
+++ server/src/middlewares/adminAuth.ts
@@ -0,0 +1,24 @@
+import type e = require("express");
+const jwt = require("jsonwebtoken");
+const adminAuth = (req: e.Request, res: e.Response, next: e.NextFunction) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decode.role !== "admin") {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+    req.adminId = decode.id;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = adminAuth;
---
+++ server/src/types/express/index.d.ts
@@ -0,0 +1,6 @@
+declare namespace Express {
+  export interface Request {
+    adminId?: string;
+    customerId?: string;
+  }
+}
