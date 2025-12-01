23372bcdf2a943de024da53440f7f052709fe116
mustofa adny <adniadni04@gmail.com>
2025-12-01 15:36:47 +0700

    feat: create customerAuth middleware for authentication

---
+++ server/src/middlewares/customerAuth.ts
@@ -0,0 +1,28 @@
+import type e = require("express");
+const jwt = require("jsonwebtoken");
+const customerAuth = (
+  req: e.Request,
+  res: e.Response,
+  next: e.NextFunction
+) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decode.role !== "customer") {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+    req.customerId = decode.id;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = customerAuth;
